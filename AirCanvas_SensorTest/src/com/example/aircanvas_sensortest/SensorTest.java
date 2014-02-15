aaafe4605afc74f3a3fdb5255e48f203cfe83443
Cheng Xing <xc561@hotmail.com>
2014-02-15 17:12:14 -0500

    sensor test

---
+++ AirCanvas_SensorTest/src/com/example/aircanvas_sensortest/SensorTest.java
@@ -0,0 +1,74 @@
+package com.example.aircanvas_sensortest;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Context;
+import android.view.Menu;
+import android.widget.TextView;
+
+public class SensorTest extends Activity implements SensorEventListener {
+	
+	private static float ALPHA = 0.01f;		// Low-pass filter smoothing constant
+	
+	private SensorManager sm;
+	private Sensor accel;
+	private float[] accel_data = new float[3];
+	private float[] vel_data = new float[3];
+	
+	private boolean zero = true;
+	
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_sensor_test);
+		
+		sm = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+		accel = sm.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.sensor_test, menu);
+		return true;
+	}
+	
+	@Override
+	public void onResume() {
+		super.onResume();
+		sm.registerListener(this, accel, SensorManager.SENSOR_DELAY_GAME);
+	}
+	
+	public void onSensorChanged(SensorEvent event) {
+		// low pass filter
+		if (zero) {
+			accel_data = event.values;
+			zero = false;
+		} else {
+			for (int i = 0; i < accel_data.length; i++)
+				event.values[i] = ALPHA * event.values[i] - (1.0f - ALPHA) * accel_data[i];
+			accel_data = event.values;
+		}
+
+		// velocity
+		for (int i = 0; i < vel_data.length; i++) {
+			vel_data[i] += event.values[i];
+		}
+		
+		String a = "Accel Data:\nX: " + event.values[0] + "\nY: " + event.values[1] + "\nZ: " + event.values[2] + "\n\n";
+		String v = "Velocity Data:\nX: " + vel_data[0] + "\nY: " + vel_data[1] + "\nZ: " + vel_data[2] + "\n\n";
+		((TextView) this.findViewById(R.id.accel_data)).setText(a+v);
+	}
+
+	@Override
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+		
+	}
+
+}
